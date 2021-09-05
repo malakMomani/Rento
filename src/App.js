633f6d68b4dce7102ee3428e27bdcd7c05bb8bd7
Malak Al-Momani <malak.momani.96@gmail.com>
2021-09-05 22:33:24 +0300

    add header ahbal

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './basics/Header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <p>WELCOME</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
